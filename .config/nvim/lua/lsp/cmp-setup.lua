d16513500c6809df19b1f5fa2e985b00b7790235
abhamra <arjun.bhamra25@gmail.com>
2024-05-06 00:52:44 -0400

    added autocomplete to nvim setupg add .! + updated readme for oh my posh, zsh reminders

--- .config/nvim/lua/lsp/cmp-setup.lua
+++ .config/nvim/lua/lsp/cmp-setup.lua
@@ -1,51 +1,28 @@
+-- TODO: completely redo this entire file, from scratch, see if that helps (bitch)
+-- also set up UltiSnips
 -- [[ Configure nvim-cmp ]]
 -- See `:help cmp`
-local cmp = require 'cmp'
--- local luasnip = require 'luasnip'
---luasnip.config.setup {}
 
-cmp.setup {
+local cmp = require 'cmp'
+
+cmp.setup({
   snippet = {
     expand = function(args)
-      -- luasnip.lsp_expand(args.body)
+      require('luasnip').lsp_expand(args.body)
     end,
   },
-  completion = {
-    completeopt = 'menu,menuone,noinsert'
-  },
-  mapping = cmp.mapping.preset.insert {
+  mapping = {
     ['<C-j>'] = cmp.mapping.select_next_item(),
-    ['<C-k>'] = cmp.mapping.select_prev_item(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<C-y>'] = cmp.mapping.confirm { select = true },
-    ['<Enter>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
+    ['<C-l>'] = cmp.mapping.select_prev_item(),
+    ['<CR>']  = cmp.mapping.confirm({
       select = true,
-    },
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-        -- elseif luasnip.expand_or_locally_jumpable() then
-        --   luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-        -- elseif luasnip.locally_jumpable(-1) then
-        --   luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
+      behavior = cmp.ConfirmBehavior.Insert,
+    })
   },
-  sources = {
-    { name = 'nvim_lsp' },
-    -- { name = 'luasnip' },
-    { name = 'path' },
-  },
-}
-
--- vim: ts=2 sts=2 sw=2 et
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp', group_index = 1 },
+    { name = 'luasnip',  group_index = 1 },
+    { name = 'buffer',   group_index = 2 },
+    { name = 'path',     group_index = 2 },
+  })
+})
--- README.md
+++ README.md
@@ -36,3 +36,7 @@ $ stow .
 The idea for using stow was from the Youtube channel "Dreams of Autonomy"; their video is [here](https://www.youtube.com/watch?v=y6XCebnB9gs).
 
 Make sure to add command line stuff like fzf and bat, etc., from the video [here](https://www.youtube.com/watch?v=mmqDYw9C30I&ab_channel=JoseanMartinez).
+
+Also, note that Oh My Posh is also quite nice, and I really like how it works with the rest of the set-up!
+For future reference, try to use zsh on any new machines instead of bash; it maintains a lot of features but p10k and a lot of other native features
+make it a more attractive pick across the board.

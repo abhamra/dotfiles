ca4b1bc2570167f0e605e1e57f8a25c3bdc78dac
abhamra <arjun.bhamra25@gmail.com>
2024-05-15 18:11:12 -0400

    removed hardtime, vimtex

--- .config/nvim/lua/3thparty/plugins/hardtime.lua
+++ .config/nvim/lua/3thparty/plugins/hardtime.lua
@@ -1,9 +1,9 @@
 return {
-  {
-    "m4xshen/hardtime.nvim",
-    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-    opts = {}
-  },
+  -- {
+  --   "m4xshen/hardtime.nvim",
+  --   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+  --   opts = {}
+  -- },
   {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },

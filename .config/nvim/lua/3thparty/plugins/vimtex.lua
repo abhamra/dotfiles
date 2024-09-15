163267196139a8b294258f236aa7f4c385148389
Arjun Bhamra <arjun.bhamra25@gmail.com>
2024-09-15 15:37:51 -0400

    update vimtex lua

--- .config/nvim/lua/3thparty/plugins/vimtex.lua
+++ .config/nvim/lua/3thparty/plugins/vimtex.lua
@@ -11,32 +11,11 @@ return {
     end
   },
   {
-    "iurimateus/luasnip-latex-snippets.nvim",
+    "abhamra/luasnip-latex-snippets.nvim",
     -- vimtex isn't required if using treesitter
     requires = { "L3MON4D3/LuaSnip", "lervag/vimtex" },
     config = function()
-      require 'luasnip-latex-snippets'.setup()
-      -- or setup({ use_treesitter = true })
-      local ls = require("luasnip")
-      ls.config.setup { enable_autosnippets = true }
-      -- local utils = require("luasnip-latex-snippets.util.utils")
-      --
-      -- local not_math = utils.not_math() -- pass true if using Treesitter
-      -- local snip = ls.parser.parse_snippet({ trig = "tbb", name = "Bold" }, "\\textbf{$1}$0")
-      -- snip.condition = not_math
-      -- snip.priority = 10
-      --
-      -- ls.add_snippets("tex", { snip }, {
-      --   type = "autosnippets",
-      -- })
-      --
-      -- local snip2 = ls.parser.parse_snippet({ trig = "tii", name = "Italics" }, "\\textit{$1}$0")
-      -- snip2.condition = not_math
-      -- snip2.priority = 10
-      --
-      -- ls.add_snippets("tex", { snip2 }, {
-      --   type = "autosnippets",
-      -- })
+      require("luasnip").config.setup { enable_autosnippets = true }
     end,
   }
 }
